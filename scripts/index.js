875b6b7fbd04804c3116a06983137184b65f59bb
mihneavlad <mihnel@yahoo.com>
2018-06-04 16:41:43 +0200

    Created first version of working adding input field.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-// Test
+
+let addItem = document.querySelector("#shopping-list").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let line = document.createElement("li");
+    let inputValue = event.target.querySelector("#input").value;
+    let newLine = document.createTextNode(inputValue);
+
+    const list = document.querySelector("#items");
+
+    list.appendChild(newLine);
+    // list.insertBefore(line, list.querySelector(":nth-child(1)"));
+  });
