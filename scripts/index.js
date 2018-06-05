c6e1028cf0cf538b568f3a3605853a7e403ea6fa
mihneavlad <mihnel@yahoo.com>
2018-06-05 15:45:09 +0200

    First version of working list

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,24 @@
-const list = document.querySelector("#items");
+const list = document.querySelector('#items');
 
-let addItem = document.querySelector("#shopping-list").addEventListener("submit", function(event) {
-    event.preventDefault();
+let addItem = document.querySelector('#shopping-list').addEventListener('submit', function(event) {
+	event.preventDefault();
 
-    let line = document.createElement("li");
-    let inputValue = event.target.querySelector("#input").value;
-    let newLine = document.createTextNode(inputValue);
+	let line = document.createElement('li');
+	let inputValue = event.target.querySelector('#input').value;
+	let newLine = document.createTextNode(inputValue);
 
-    line.appendChild(newLine);
-    list.appendChild(line);
-    // list.insertBefore(line, list.querySelector(":nth-child(1)"));
-  });
+  line.appendChild(newLine);
+	list.appendChild(line);
+	let removeButton = document.createElement('button');
+	let textOfTheButton = document.createTextNode('Remove');
+
+	removeButton.appendChild(textOfTheButton);
+	line.appendChild(removeButton);
+
+	removeButton.addEventListener('click', function(event) {
+		event.preventDefault();
+
+		let itemRemove = event.target.parentNode;
+		list.removeChild(itemRemove);
+	});
+});
