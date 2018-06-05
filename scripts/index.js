c94514882b11916e6d2f8d0754c101ef0e4502d9
mihneavlad <mihnel@yahoo.com>
2018-06-05 18:20:29 +0200

    New bootstrap align

--- scripts/index.js
+++ scripts/index.js
@@ -7,18 +7,22 @@ let addItem = document.querySelector('#shopping-list').addEventListener('submit'
 	let inputValue = event.target.querySelector('#input').value;
 	let newLine = document.createTextNode(inputValue);
 
-  line.appendChild(newLine);
+	line.appendChild(newLine);
 	list.appendChild(line);
+
 	let removeButton = document.createElement('button');
-	let textOfTheButton = document.createTextNode('Remove');
+	let textOfTheButton = document.createTextNode('Delete');
 
 	removeButton.appendChild(textOfTheButton);
 	line.appendChild(removeButton);
+
+	removeButton.classList.add('delete-btn');
 
 	removeButton.addEventListener('click', function(event) {
 		event.preventDefault();
 
 		let itemRemove = event.target.parentNode;
 		list.removeChild(itemRemove);
+
 	});
 });
